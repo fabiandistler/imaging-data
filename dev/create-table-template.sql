a220c2d865e7eddf721b706c305521456c039092
Fabian <fabian.distler@gmx.net>
2024-12-27 15:59:10 +0100

    fix keys

--- dev/create-table-template.sql
+++ dev/create-table-template.sql
@@ -1,30 +1,25 @@
--- Active: 1735046923923@@127.0.0.1@3306
--- Create Session Table
-CREATE TABLE Session (
+CREATE TABLE Sessioninfo (
     session_id INTEGER PRIMARY KEY,
     mouse_id TEXT NOT NULL,
-    session_type TEXT NOT NULL
+    session_type TEXT NOT NULL,
+    UNIQUE (mouse_id, session_type)
 );
 
--- Create Behavior Table
 CREATE TABLE Behavior (
-    frame_id INTEGER PRIMARY KEY,
     frame_number INTEGER NOT NULL,
     session_id INTEGER NOT NULL,
     running_speed REAL NOT NULL,
-    eye_diameter REAL NOT NULL,
     stimulus_type TEXT NOT NULL,
-    -- This foreign key constraint ensures that each session_id in the Behavior table
-    -- corresponds to a valid session_id in the Session table, maintaining referential integrity.
-    FOREIGN KEY (session_id) REFERENCES Session (session_id)
+    PRIMARY KEY (frame_number, session_id),
+    FOREIGN KEY (session_id) REFERENCES Sessioninfo (session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create Components Table
 CREATE TABLE Components (
-    component_id INTEGER NOT NULL,
-    frame_id INTEGER NOT NULL,
+    component_id INTEGER PRIMARY KEY,
     onset_value INTEGER NOT NULL,
-    PRIMARY KEY (component_id, frame_id),
+    frame_number INTEGER NOT NULL,
+    session_id INTEGER NOT NULL,
     -- The ON DELETE CASCADE clause ensures that deleting a row in Behavior also deletes corresponding rows in Components
-    FOREIGN KEY (frame_id) REFERENCES Behavior (frame_id) ON DELETE CASCADE
+    FOREIGN KEY (frame_number, session_id) REFERENCES Behavior (frame_number, session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
