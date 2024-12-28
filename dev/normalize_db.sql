a719a2802b97de04946b2dbe297308af18e8621d
Fabian <fabian.distler@gmx.net>
2024-12-28 17:06:51 +0100

    fix type and add data input

--- dev/normalize_db.sql
+++ dev/normalize_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE recording (
-    recording_id TEXT PRIMARY KEY,
+    recording_id INTEGER PRIMARY KEY,
     recording_type TEXT NOT NULL
 );
 
 CREATE TABLE mouse (mouse_id TEXT PRIMARY KEY);
 
 CREATE TABLE recording_per_mouse (
-    recording_id TEXT NOT NULL,
+    recording_id INTEGER NOT NULL,
     mouse_id TEXT NOT NULL,
     PRIMARY KEY (recording_id, mouse_id),
     FOREIGN KEY (recording_id) REFERENCES recording (recording_id),
@@ -15,7 +15,7 @@ CREATE TABLE recording_per_mouse (
 
 CREATE TABLE frame (
     frame_id INTEGER PRIMARY KEY,
-    recording_id TEXT NOT NULL,
+    recording_id INTEGER NOT NULL,
     frame_number INTEGER NOT NULL,
     running_speed REAL NOT NULL,
     stimulus_id INTEGER NOT NULL,
@@ -34,3 +34,59 @@ CREATE TABLE stimulus (
     stimulus_id INTEGER PRIMARY KEY AUTOINCREMENT,
     stimulus_type TEXT NOT NULL
 );
+
+
+
+
+-- Transfer data to new tables
+
+-- Insert unique recordings
+INSERT INTO recording (recording_id, recording_type)
+SELECT DISTINCT session_id AS recording_id, session_type AS recording_type
+FROM frame_level_data;
+
+-- Insert unique mice
+INSERT INTO
+    mouse (mouse_id)
+SELECT DISTINCT
+    mouse_id
+FROM frame_level_data;
+
+-- Insert recording per mouse
+INSERT INTO recording_per_mouse (recording_id, mouse_id)
+SELECT DISTINCT session_type AS recording_id, mouse_id
+FROM frame_level_data;
+
+-- Insert unique stimuli
+INSERT INTO
+    stimulus (stimulus_type)
+SELECT DISTINCT
+    stimulus_type
+FROM frame_level_data;
+
+-- Insert frames
+INSERT INTO
+    frame (
+        frame_id,
+        recording_id,
+        frame_number,
+        running_speed,
+        stimulus_id
+    )
+SELECT f.frame_id, f.session_type AS recording_id, f.frame_number, f.running_speed, s.stimulus_id
+FROM
+    frame_level_data f
+    JOIN stimulus s ON f.stimulus_type = s.stimulus_type;
+
+-- Insert components
+INSERT INTO
+    component (
+        component_id,
+        frame_id,
+        onset_value
+    )
+SELECT
+    component_id,
+    frame_id,
+    onset_value
+FROM frame_level_data;
