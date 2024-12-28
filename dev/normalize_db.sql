131bfc9178a6ed3b93d984db1085bfd56788d45e
Fabian <fabian.distler@gmx.net>
2024-12-28 15:10:39 +0100

    normalize

--- dev/normalize_db.sql
+++ dev/normalize_db.sql
@@ -1,27 +1,30 @@
-CREATE TABLE frame_level_data (
+CREATE TABLE recording (
+    recording_id TEXT PRIMARY KEY,
+    recording_type TEXT NOT NULL
+);
+
+CREATE TABLE mouse (mouse_id TEXT PRIMARY KEY);
+
+CREATE TABLE recording_per_mouse (
+    recording_id TEXT NOT NULL,
+    mouse_id TEXT NOT NULL,
+    PRIMARY KEY (recording_id, mouse_id),
+    FOREIGN KEY (recording_id) REFERENCES recording(recording_id),
+    FOREIGN KEY (mouse_id) REFERENCES mouse(mouse_id)
+);
+
+CREATE TABLE frame (
     frame_id INTEGER PRIMARY KEY,
-    
-    
+    recording_id TEXT NOT NULL,
     frame_number INTEGER NOT NULL,
     running_speed REAL NOT NULL,
     stimulus_type TEXT NOT NULL,
-    component_id INTEGER NOT NULL,
-    onset_value INTEGER NOT NULL
+    FOREIGN KEY (recording_id) REFERENCES recording(recording_id)
 );
 
-CREATE TABLE mouse (
-mouse_id TEXT PRIMARY KEY
-
-);
-
-CREATE TABLE session (
-session_id TEXT PRIMARY KEY,
-session_type TEXT NOT NULL
-
-);
-
-CREATE TABLE session_per_mouse (
-session_id TEXT PRIMARY KEY,
-mouse_id TEXT PRIMARY KEY
-
+CREATE TABLE component (
+    component_id INTEGER PRIMARY KEY,
+    frame_id INTEGER NOT NULL,
+    onset_value INTEGER NOT NULL,
+    FOREIGN KEY (frame_id) REFERENCES frame(frame_id)
 );
