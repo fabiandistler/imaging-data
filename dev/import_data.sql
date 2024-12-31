f37742f039766f03fe5a211731b8f1525c6eeb37
Fabian <fabian.distler@gmx.net>
2024-12-31 15:04:31 +0100

    merged

--- dev/import_data.sql
+++ dev/import_data.sql
@@ -1,3 +1,19 @@
+CREATE TABLE temp_table (
+    frame_id INTEGER PRIMARY KEY,
+    mouse_id TEXT NOT NULL,
+    session_type TEXT NOT NULL,
+    frame_number INTEGER NOT NULL,
+    running_speed REAL NOT NULL,
+    stimulus_type TEXT NOT NULL,
+    component_id INTEGER NOT NULL,
+    onset_value INTEGER NOT NULL
+);
+
+
+-- Run import.py
+
+-- Normalize data base
+
 CREATE TABLE recording (
     recording_id INTEGER PRIMARY KEY,
     recording_type TEXT NOT NULL 
